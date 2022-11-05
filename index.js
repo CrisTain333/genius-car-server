ce669238bb8522434e81530f182a62976d133c0b
Cristain <mitashil99@gmail.com>
2022-11-05 10:11:48 +0400

    project completed

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const { MongoClient, ObjectId } = require("mongodb");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
-require('dotenv').config()
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Server On Fire");
 });
 
-// const uri = `mongodb://localhost:27017/`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@practicebaba.aon4ndq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
+
+
+
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader){
+      return res.status(401).send({message: 'unauthorized access'});
+  }
+  const token = authHeader.split(' ')[1];
+  console.log(token)
+
+  jwt.verify(token, process.env.JWT_SECRET, function(err, decoded){
+      if(err){
+          return res.status(403).send({message: 'Forbidden access'});
+      }
+      console.log(decoded)
+      req.decoded = decoded;
+      next();
+  })
+}
+
 
 const run = async () => {
   try {
@@ -23,19 +45,33 @@ const run = async () => {
     const poroductColation = client.db("geniusCar").collection("products");
     const blogPostColation = client.db("geniusCar").collection("blogPost");
 
+
+
+    app.post('/jwt', (req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '5h'})
+      res.send({token})
+    })  
+    
+
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
+
+
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const cursor = await serviceCollection.findOne(query);
       res.send(cursor);
     });
-    app.get("/orders", async (req, res) => {
+
+    app.get("/orders",verifyJWT,async (req, res) => {
+   
+     
       let query = {};
       if (req.query.email) {
         query = {
@@ -44,7 +80,6 @@ const run = async () => {
       }
       const result = orderCollection.find(query);
       const orders = await result.toArray();
-      console.log(orders);
       res.send(orders);
     });
 
@@ -61,6 +96,24 @@ const run = async () => {
       res.send(result);
     });
 
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
     app.get("/products", async (req, res) => {
       const query = {};
       const result = poroductColation.find(query);
@@ -70,27 +123,25 @@ const run = async () => {
 
     app.post("/blog/post", async (req, res) => {
       const blogPost = req.body;
-      console.log(blogPost)
+      console.log(blogPost);
       const result = await blogPostColation.insertOne(blogPost);
-      console.log(result)
+      console.log(result);
       res.send(result);
     });
 
-    app.get('/blogs',async(req,res)=>{
-        const query = {}
-        const result = blogPostColation.find(query)
-        const blogPost = await result.toArray()
-        res.send(blogPost);
-    })
+    app.get("/blogs", async (req, res) => {
+      const query = {};
+      const result = blogPostColation.find(query);
+      const blogPost = await result.toArray();
+      res.send(blogPost);
+    });
 
-    app.get('/blog/:id',async(req,res)=>{
-        const id =  req.params.id
-        const query = {_id:ObjectId(id)}
-        const result = await  blogPostColation.findOne(query);
-        res.send(result);
-    })
-
-
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await blogPostColation.findOne(query);
+      res.send(result);
+    });
   } finally {
   }
 };
